081c4eea6069dcd74594df123b9554a7c4b88d9d
Yasin Can <canyasin1996@gmail.com>
2022-09-12 14:44:30 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,15 @@ function App() {
 
   useEffect(() => {
     setLoginLoading(true);
+    const storageTheme = localStorage.getItem("theme");
+    if (storageTheme) {
+      setTheme(storageTheme);
+    }
+    setLoginLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setLoginLoading(true);
     const storageUsername = localStorage.getItem("username");
     if (storageUsername) {
       setSavedUsername(storageUsername);
@@ -32,8 +41,8 @@ function App() {
   }, []);
 
   useEffect(() => {
+    setLoginLoading(true);
     async function getTodos() {
-      setLoginLoading(true);
       try {
         const response = await axios.get(
           `https://631462d5fc9dc45cb4ecb7bb.mockapi.io/todos`
@@ -51,15 +60,6 @@ function App() {
     localStorage.setItem("username", username);
     setSavedUsername(username);
   };
-
-  useEffect(() => {
-    setLoginLoading(true);
-    const storageTheme = localStorage.getItem("theme");
-    if (storageTheme) {
-      setTheme(storageTheme);
-    }
-    setLoginLoading(false);
-  }, []);
 
   const isLight = theme === "light";
 
@@ -164,8 +164,10 @@ function App() {
   };
 
   return (
-    <ThemeContext.Provider value={{ theme }} style={{ position: "relative" }}>
-      {savedUsername ? (
+    <ThemeContext.Provider value={{ isLight, toggleTheme }}>
+      {loginLoading ? (
+        <Loading />
+      ) : savedUsername ? (
         <div className="container" id={theme}>
           <div className="welcome-text-add-todo" id={theme}>
             <div className="welcome-text-theme-mode" id={theme}>
@@ -217,20 +219,70 @@ function App() {
           </div>
         </div>
       ) : (
-        <div className="login-container" id={theme}>
-          {loginLoading ? (
-            <Login
-              handleChange={setUsername}
-              handleLogin={handleLogin}
-              value={username}
-            />
-          ) : (
-            <Loading />
-          )}
-        </div>
+        <Login
+          handleChange={setUsername}
+          handleLogin={handleLogin}
+          value={username}
+        />
       )}
     </ThemeContext.Provider>
   );
 }
 
 export default App;
+
+/* {loginLoading && <Loading />}
+{savedUsername ? (
+  <div className="container" id={theme}>
+    <div className="welcome-text-add-todo" id={theme}>
+      <div className="welcome-text-theme-mode" id={theme}>
+        <div className="welcome-heading">
+          <h1 className="welcome-text">Welcome {savedUsername}</h1>
+        </div>
+        <div className="theme-buttons">
+          <button className="mode-btn" onClick={toggleTheme}>
+            <i
+              className={`fa-${
+                theme === "light" ? "regular" : "solid"
+              } fa-lightbulb`}
+            ></i>
+          </button>
+        </div>
+      </div>
+      <div className="add-todo" id={theme}>
+        <AddTodo
+          handleChange={setNewTask}
+          addTask={addTask}
+          value={newTask}
+          addLoading={addLoading}
+        />
+      </div>
+    </div>
+    <div className="todo-component" id={theme}>
+      <div className="todos" id={theme}>
+        {todoList.map((task) => {
+          return (
+            <Todo
+              cancelEdit={cancelEdit}
+              todoEditing={todoEditing}
+              setTodoEditing={setTodoEditing}
+              setTodoLoading={setTodoLoading}
+              editingText={editingText}
+              setEditingText={setEditingText}
+              completeTask={completeTask}
+              editTask={editTask}
+              deleteTask={deleteTask}
+              isCompleted={task.isCompleted}
+              key={task.id}
+              content={task.content}
+              id={task.id}
+              loading={todoLoading === task.id}
+            />
+          );
+        })}
+      </div>
+    </div>
+  </div>
+) : (
+
+)} */
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,6 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="loading-screen">
-      <h2>Loading Your App</h2>
       <ReactLoading type="balls" color="#201f1f4b" height={100} width={50} />
     </div>
   );
